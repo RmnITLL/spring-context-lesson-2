94e295cc162eb1ad3cf07bbbe04fb8c12322366d
https://github.com/RmnITLL/servlets_lesson-1 <inetmilo@rambler.ru>
2022-12-06 17:30:19 +0300

    set for OrderService

--- src/main/java/ru/gb/context/OrderService.java
+++ src/main/java/ru/gb/context/OrderService.java
@@ -1,12 +1,25 @@
 package ru.gb.context;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderService {
 
+    private Productservice productservice;
+
+    @Autowired
+    public void setProductservice(Productservice productservice) {
+        this.productservice = productservice;
+    }
+
+//    @Autowired
+//    public OrderService(Productservice productservice) {
+//        this.productservice = productservice;
+//    }
+
     public void createOrderFromProduct(Long productId) {
         System.out.println("Заказ создан:");
-        System.out.println("Продукт №" + productId);
+        System.out.println(productservice.getTitleById(productId));
     }
 }
